fa0f508c6b2a4eb8d3479e01f458d1f52a3395c9
Jasmin Kaset <kasetski@gmail.com>
2020-10-16 15:01:24 -0500

    created CriminalList function

---
+++ scripts/criminals/criminalList.js
@@ -0,0 +1,22 @@
+import {getCriminals, useCriminals} from "./CriminalProvider.js"
+import {criminalCard} from "./criminal.js"
+
+export const CriminalList = () => {
+  
+  getCriminals().then( 
+    () => {
+      const criminalContainer = document.querySelector(".criminalsContainer")
+      const criminalArray = useCriminals() 
+      let buildCriminalList = ""
+      for (const criminalOne of criminalArray) {
+        buildCriminalList += criminalCard(criminalOne)
+    }
+    
+  
+  
+  criminalContainer.innerHTML += `<div>${buildCriminalList}</div>`
+})
+}
+
+//console.log(criminalArray)
+//return criminalArray
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log("Welcome to the main module")
+// import {getOfficers, useOfficers} from "./officers/OfficerProvider.js"
+// getOfficers()
+// .then(() => {
+//   const officersArray = useOfficers()
+//   console.log(officersArray)
+// })
+
+// import {getCriminals, useCriminals} from "./criminals/CriminalProvider.js"
+// getCriminals()
+// .then(() => {
+//   const criminalArray = useCriminals()
+//   console.log(criminalArray)
+// })
+
+import {CriminalList} from "./criminals/criminalList.js"
+CriminalList()
