dcb0f8698912aca90aafeb38bab64054a7507ad5
Jasmin Kaset <kasetski@gmail.com>
2020-10-22 13:50:29 -0500

    rendered to dom

---
+++ scripts/notes/Note.js
@@ -0,0 +1,16 @@
+//html converter
+
+export const NoteAsHTML = (noteObj) => {
+//convert obj to html
+//return that HTML string
+  return `
+  <section class="note__card">
+  <h3>Note:</h3>
+  <p>Author: ${noteObj.author}</p>
+  <p>Suspect: ${noteObj.suspect}</p>
+  <p>Note: ${noteObj.note}</p>
+  <p>Date of Interview: ${noteObj.dateOfInterview}</p>
+  <p>Timestamp: ${new Date(noteObj.timestamp).toLocaleDateString('en-US')}</p>
+</section>
+  `
+}
---
+++ scripts/notes/NoteList.js
@@ -0,0 +1,32 @@
+//get notes from api
+
+//use the notes array 
+
+//interate notes to make html representation
+
+//render html representation to the dom
+import {NoteAsHTML} from "./Note.js"
+import {getNotes, useNotes} from "./NoteDataProvider.js"
+const notesContainer = document.querySelector(".notesContainer")
+
+export const NoteList = () => {
+  getNotes()
+  .then(() => {
+  const allNotes = useNotes()
+  //console.log("allNotes:", allNotes)
+  render(allNotes)
+  })
+}
+
+const render = (notesArray) => {
+  let notesHTMLRepresentations = ""
+  for (const note of notesArray) {
+
+    notesHTMLRepresentations += NoteAsHTML(note)
+  }
+    notesContainer.innerHTML = `  
+            ${notesHTMLRepresentations}
+          
+        `
+  
+  }
